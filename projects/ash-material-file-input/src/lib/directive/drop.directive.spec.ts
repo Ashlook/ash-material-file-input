c5b1c03347ca3d73e6c6f1fb0d6320826d4be5fa
Slvn Fbvr <sylvain.febvre@gmail.com>
2020-04-27 13:52:50 +0200

    test

--- projects/ash-material-file-input/src/lib/directive/drop.directive.spec.ts
+++ projects/ash-material-file-input/src/lib/directive/drop.directive.spec.ts
@@ -1,8 +1,44 @@
 import { DropDirective } from './drop.directive';
+import { Component, ViewChild, Input, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
-describe('DropDirective', () => {
+/* describe('DropDirective', () => {
   it('should create an instance', () => {
     const directive = new DropDirective();
     expect(directive).toBeTruthy();
   });
-});
+}); */
+
+@Component({
+  selector: 'test-drop',
+  template: `<div ashDrop dragoverClass="test-class"></div>`,
+})
+class TestHostComponent {
+  @ViewChild(DropDirective) directive!: DropDirective;
+}
+
+fdescribe('DropDirective', () => {
+  let host: TestHostComponent;
+  let hostElement: DebugElement;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let directive: DropDirective;
+
+  beforeEach(async(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [
+        TestHostComponent,
+        DropDirective,
+      ],
+    }).createComponent(TestHostComponent);
+
+    fixture.detectChanges();
+
+    hostElement = fixture.debugElement;
+    host = fixture.componentInstance;
+    directive = host.directive;
+  }));
+
+  it('should create an instance', () => {
+    expect(directive).toBeTruthy()
+  })
+})
