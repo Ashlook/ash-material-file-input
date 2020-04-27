32187b4b1e4cfa7e7fdabc24fe6b140b93dc871f
Slvn Fbvr <sylvain.febvre@gmail.com>
2020-04-27 12:13:37 +0200

    Add component

---
+++ projects/example/src/app/file-input/file-input.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-input',
+  templateUrl: './file-input.component.html',
+  styleUrls: ['./file-input.component.scss']
+})
+export class FileInputComponent implements OnInit {
+
+  get basicCode() {
+    return `    <mat-form-field>
+        <mat-label>Click me to chose a file</mat-label>
+        <ash-mat-file-input></ash-mat-file-input>
+        <mat-icon color="accent" matSuffix>publish</mat-icon>
+    </mat-form-field>
+
+    <mat-form-field>
+        <mat-label>Drop a file in the container</mat-label>
+        <ash-mat-file-input fileDrop>
+            <label dropLabel class="example-label">
+                <mat-icon color="accent">publish</mat-icon> Custom label
+            </label>
+        </ash-mat-file-input>
+    </mat-form-field>`
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
