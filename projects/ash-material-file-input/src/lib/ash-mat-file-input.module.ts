62038c0deb3058ff9fb4709be971019e81fcadd9
Slvn Fbvr <sylvain.febvre@gmail.com>
2020-04-27 12:08:18 +0200

    DropLabelDirective

--- projects/ash-material-file-input/src/lib/ash-mat-file-input.module.ts
+++ projects/ash-material-file-input/src/lib/ash-mat-file-input.module.ts
@@ -3,12 +3,13 @@ import { FileInputComponent } from './file-input/file-input.component';
 import { ByteFormatPipe } from './pipe/byte-format.pipe';
 import { DropDirective } from './directive/drop.directive';
 import { BrowserModule } from '@angular/platform-browser';
+import { DropLabel } from './file-input/drop-label';
 
 
 
 @NgModule({
-  declarations: [FileInputComponent, ByteFormatPipe, DropDirective],
+  declarations: [FileInputComponent, ByteFormatPipe, DropDirective, DropLabel],
   imports: [BrowserModule],
-  exports: [FileInputComponent, ByteFormatPipe, DropDirective],
+  exports: [FileInputComponent, ByteFormatPipe, DropDirective, DropLabel],
 })
 export class AshMatFileInputModule { }
