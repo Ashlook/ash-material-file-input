ec8a19c264ab607c9b1406be480fa806c4756807
Slvn Fbvr <sylvain.febvre@gmail.com>
2020-04-27 12:13:03 +0200

    add used module

--- projects/example/src/app/app.module.ts
+++ projects/example/src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {} from 'material-file-input'
+
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AshMatFileInputModule } from 'ash-material-file-input';
+import { SharedMaterialModule } from './shared-material.module';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { FileInputComponent } from './file-input/file-input.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent,
+    FileInputComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
+    AshMatFileInputModule,
+    ReactiveFormsModule,
+    SharedMaterialModule,
     BrowserModule,
-
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
