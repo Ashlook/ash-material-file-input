4ad02a4660ae4c076fcd10328669dfc9ca24513a
Slvn Fbvr <sylvain.febvre@gmail.com>
2020-04-28 16:26:13 +0200

    Create file-validator.spec.ts (#5)

---
+++ projects/ash-material-file-input/src/lib/validator/file-validator.spec.ts
@@ -0,0 +1,31 @@
+import { FileValidators } from './file-validator';
+import { FormControl } from '@angular/forms';
+
+class FileListMock implements FileList {
+  readonly length: number;
+  [index: number]: File;
+
+  constructor(files: File[]) {
+    files.forEach((file, i) => this[i] = file);
+    this.length = files.length;
+  }
+
+  item(index: number) {
+    return this[index];
+  }
+}
+
+describe('FileValidators', () => {
+  const fileTxt = new File([''], 'text.txt', {type: 'text/plain'});
+  const fileImg = new File([''], 'text.png', {type: 'image/png'});
+
+  describe('fileExtension', () => {
+
+    it('should validate', () => {
+      const data = new FileListMock([fileTxt, fileTxt, fileTxt]);
+      const control = new FormControl(data, [FileValidators.FileExtOk('.txt')]);
+      expect(control.value).toBe(data);
+      expect(control.valid).toBeTruthy();
+    });
+  });
+});
