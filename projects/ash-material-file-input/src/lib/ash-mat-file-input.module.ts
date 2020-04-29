16a100a0d60622547b4836d17a023237e3909633
Slvn Fbvr <sylvain.febvre@gmail.com>
2020-04-29 12:40:39 +0200

    Add NoMultipleDirective

--- projects/ash-material-file-input/src/lib/ash-mat-file-input.module.ts
+++ projects/ash-material-file-input/src/lib/ash-mat-file-input.module.ts
@@ -3,12 +3,13 @@ import { FileInputComponent } from './file-input/file-input.component';
 import { ByteFormatPipe } from './pipe/byte-format.pipe';
 import { DropDirective } from './directive/drop.directive';
 import { BrowserModule } from '@angular/platform-browser';
+import { NoMultipleDirective } from './directive/no-multiple.directive';
 
 
 
 @NgModule({
-  declarations: [FileInputComponent, ByteFormatPipe, DropDirective],
+  declarations: [FileInputComponent, ByteFormatPipe, DropDirective, NoMultipleDirective],
   imports: [BrowserModule],
-  exports: [FileInputComponent, ByteFormatPipe, DropDirective],
+  exports: [FileInputComponent, ByteFormatPipe, DropDirective, NoMultipleDirective],
 })
 export class AshFileInputModule { }
