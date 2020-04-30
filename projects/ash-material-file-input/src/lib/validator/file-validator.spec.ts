3ba6b97d02cfa5396f8786fa979bdfe931d33123
Slvn Fbvr <sylvain.febvre@gmail.com>
2020-04-30 16:45:58 +0200

    Test for acceptedExtensions validator (#5)

--- projects/ash-material-file-input/src/lib/validator/file-validator.spec.ts
+++ projects/ash-material-file-input/src/lib/validator/file-validator.spec.ts
@@ -16,16 +16,43 @@ class FileListMock implements FileList {
 }
 
 describe('FileValidators', () => {
-  const fileTxt = new File([''], 'text.txt', {type: 'text/plain'});
-  const fileImg = new File([''], 'text.png', {type: 'image/png'});
+  const fileTxt = new File(['123456'], 'text.txt', {type: 'text/plain'});
+  const filePng = new File([''], 'img.png', {type: 'image/png'});
+  const fileJpg = new File([''], 'img.jpg', {type: 'image/jpg'});
 
-  describe('fileExtension', () => {
+  describe('acceptedExtensions', () => {
+
+    it('should validate with no file', () => {
+       const control = new FormControl(undefined, [FileValidators.acceptedExtensions('.txt')]);
+       expect(control.value).toBe(null);
+       expect(control.valid).toBeTruthy();
+    });
 
     it('should validate', () => {
       const data = new FileListMock([fileTxt, fileTxt, fileTxt]);
       const control = new FormControl(data, [FileValidators.acceptedExtensions('.txt')]);
       expect(control.value).toBe(data);
       expect(control.valid).toBeTruthy();
+    });
+
+    it('should validate with a list of accepted extensions', () => {
+      const data = new FileListMock([filePng, filePng, fileTxt]);
+      const control = new FormControl(data, [FileValidators.acceptedExtensions('.txt,.png')]);
+      expect(control.value).toBe(data);
+      expect(control.valid).toBeTruthy();
+    });
+
+    it('should not validate with "extension" error', () => {
+      const data = new FileListMock([fileJpg]);
+      const control = new FormControl(data, [FileValidators.acceptedExtensions('.png,      .txt')]);
+      expect(control.value).toBe(data);
+      expect(control.valid).toBeFalsy();
+      const extError = control.errors.extension;
+      expect(extError).toEqual({
+        accepted: '.png,.txt',
+        current: '.jpg',
+        filename: 'img.jpg',
+      });
     });
   });
 });
