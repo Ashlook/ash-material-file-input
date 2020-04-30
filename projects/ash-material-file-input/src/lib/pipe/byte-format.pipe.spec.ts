705bc7d293a3d6986902608a095e7be9c3c763a2
Slvn Fbvr <sylvain.febvre@gmail.com>
2020-04-30 18:15:54 +0200

    ByteFormatPipe test done (#3)

--- projects/ash-material-file-input/src/lib/pipe/byte-format.pipe.spec.ts
+++ projects/ash-material-file-input/src/lib/pipe/byte-format.pipe.spec.ts
@@ -1,8 +1,46 @@
 import { ByteFormatPipe } from './byte-format.pipe';
+import { async, TestBed } from '@angular/core/testing';
 
 describe('ByteFormatPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ByteFormatPipe();
-    expect(pipe).toBeTruthy();
+  let pipe: ByteFormatPipe;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ByteFormatPipe],
+    });
+
+    pipe = TestBed.inject(ByteFormatPipe);
   });
+
+  it('should format from Byte to Megabyte (default)', () => {
+    const text = pipe.transform(104857600);
+    expect(text).toBe('100Mb');
+  });
+
+  it('should not tranform value other than number', () => {
+    const text = pipe.transform('100Mb');
+    expect(text).toBe('100Mb');
+  });
+
+  it('should transform form Kilobyte to Megabyte', () => {
+    const text = pipe.transform(102400, 'Kb');
+    expect(text).toBe('100Mb');
+  });
+
+  it('should transform from Megabyte to Byte', () => {
+    const text = pipe.transform(100, 'Mb', 'b');
+    expect(text).toBe('104857600b');
+  });
+
+  it('should tranform floating value', () => {
+    const text = pipe.transform(1.5, 'Mb', 'Kb');
+    expect(text).toBe('1536Kb');
+  });
+
+  it('should throw SyntaxError on wrong parameters', () => {
+    expect(() => pipe.transform(102400, 1)).toThrow(new SyntaxError('Wrong params format'));
+    expect(() => pipe.transform(102400, 'Kb', 'zqsd')).toThrow(new SyntaxError('Wrong params format'));
+    expect(() => pipe.transform(102400, 'gds', 12)).toThrow(new SyntaxError('Wrong params format'));
+  });
+
 });
