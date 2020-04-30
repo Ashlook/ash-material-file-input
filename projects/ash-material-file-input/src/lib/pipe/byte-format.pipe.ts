0b0b126cf46ea0003116212e404b23a6073e0e21
Slvn Fbvr <sylvain.febvre@gmail.com>
2020-04-30 18:00:34 +0200

    Tranform only number

--- projects/ash-material-file-input/src/lib/pipe/byte-format.pipe.ts
+++ projects/ash-material-file-input/src/lib/pipe/byte-format.pipe.ts
@@ -8,14 +8,13 @@ export class ByteFormatPipe implements PipeTransform {
   private _byteTypes = ['b', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb'];
 
   transform(value: any, ...args: any[]): string {
-    const v = parseFloat(value);
-    if (isNaN(v) || v < 0) {
-      return value;
+    if (isNaN(value) || value < 0) {
+      return `${value}`;
     }
     const from = args[0] || 'b';
     const to = args[1] || 'Mb';
     if (from === to) {
-      return `${v}${to}`;
+      return `${value}${to}`;
     }
     const iFrom = this._byteTypes.indexOf(from);
     const iTo = this._byteTypes.indexOf(to);
@@ -24,9 +23,9 @@ export class ByteFormatPipe implements PipeTransform {
     }
     const exp = this._byteTypes.indexOf(from) - this._byteTypes.indexOf(to);
     if (exp < 0) {
-      return parseFloat((v / Math.pow(1024, (exp * -1))).toFixed(2)) + to;
+      return parseFloat((value / Math.pow(1024, (exp * -1))).toFixed(2)) + to;
     } else {
-      return parseFloat((v * Math.pow(1024, exp)).toFixed(2)) + to;
+      return parseFloat((value * Math.pow(1024, exp)).toFixed(2)) + to;
     }
 
 
