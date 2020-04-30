0c2dea111ad270b5ca0cccce695a3011640b0117
Slvn Fbvr <sylvain.febvre@gmail.com>
2020-04-30 02:05:49 +0200

    (fix) Change BrowserModule to CommonModule

--- projects/ash-material-file-input/src/lib/ash-mat-file-input.module.ts
+++ projects/ash-material-file-input/src/lib/ash-mat-file-input.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { FileInputComponent } from './file-input/file-input.component';
 import { ByteFormatPipe } from './pipe/byte-format.pipe';
 import { DropDirective } from './directive/drop.directive';
-import { BrowserModule } from '@angular/platform-browser';
 import { NoMultipleDirective } from './directive/no-multiple.directive';
+import { CommonModule } from '@angular/common';
 
 
 
 @NgModule({
   declarations: [FileInputComponent, ByteFormatPipe, DropDirective, NoMultipleDirective],
-  imports: [BrowserModule],
+  imports: [CommonModule],
   exports: [FileInputComponent, ByteFormatPipe, DropDirective, NoMultipleDirective],
 })
 export class AshFileInputModule { }
